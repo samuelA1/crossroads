1cfcdfce9a6945e534d4af406ee083ead0f48b9a
Samuel Arrey Essim Bate Enyinaya <samuele_con@c-hit.com>
2021-02-16 14:51:42 -0500

    Added service as a provoder to the module

--- crossroads/src/app/app.module.ts
+++ crossroads/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceService } from './service.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
